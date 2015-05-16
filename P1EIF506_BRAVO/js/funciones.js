3e2f2b69b654fdbb37de73729069571d8b946ef4
mauricio0292 <mauricio0292@gmail.com>
2015-05-16 13:51:50 -0600

    validar email
    validar.css, jquery.validate.js, funciones.js

--- P1EIF506_BRAVO/js/funciones.js
+++ P1EIF506_BRAVO/js/funciones.js
@@ -1,5 +1,22 @@
 //comprueba que el sitio cargue por completo
 $('document').ready(function() {
-	//inicializa  tooltip
+    //inicializa  tooltip
     $('[data-toggle="tooltip"]').tooltip()
+
+    var elements = $("input[type!='submit'], textarea, select");
+    elements.focus(function() {
+        $(this).parents('li').addClass('highlight');
+    });
+    elements.blur(function() {
+        $(this).parents('li').removeClass('highlight');
+    });
+
+    /*$("#forgotpassword").click(function() {
+			$("#password").removeClass("required");
+			$("#main-contact-form").submit();
+			$("#password").addClass("required");
+			return false;
+		});*/
+
+    $("#main-contact-form").validate()
 });
